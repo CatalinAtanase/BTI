9524598dcdc5db811766493e5adbe68f2c071c1e
catalinAtanase <atanasecatalin20@gmail.com>
2019-10-24 19:27:03 +0300

    done code and decode

--- js/functions_hamming.js
+++ js/functions_hamming.js
@@ -1,16 +1,17 @@
 let wrongBits = []
+let messages = []
 
 function isPowerOfTwo(number) {
     return Math.log2(number) % 1 === 0;
 }
 
 function getNumberOfOnes(index, array) {
-    console.log(`index = ${index}`);
     step = index + 1
-    console.log(`step = ${step}`);
     valuesToCheck = step
     valuesToSkip = step
     numberOfOnes = 0
+    nameOfVars = ''
+    valueOfVars = ''
 
     let skip = false
 
@@ -25,11 +26,24 @@ function getNumberOfOnes(index, array) {
             continue
         }
 
+        if (nameOfVars != '') {
+            nameOfVars += ' &#8853; '
+            valueOfVars += ' &#8853; '
+        }
+
         if (!isPowerOfTwo(i + 1) || i == index) {
             if (array[i] == 1) {
                 numberOfOnes++
             }
-            console.log(`value for ${i + 1} = ${array[i]}`)
+
+            if (i == index) {
+                nameOfVars += `c<sub>${i + 1}</sub>`
+            } else {
+                nameOfVars += `a<sub>${i + 1}</sub>`
+            }
+
+            valueOfVars += `${array[i]}`
+
             valuesToCheck--
         }
 
@@ -42,7 +56,10 @@ function getNumberOfOnes(index, array) {
     if (numberOfOnes % 2 == 1) {
         wrongBits.push(step)
     }
-    console.log(`${numberOfOnes}`);
+
+    let msj = `<p> ${nameOfVars} = 0 => ${valueOfVars} = 0 (${numberOfOnes % 2 == 0 ? 'A' : 'F'}) </p>`
+
+    messages.push(msj)
 }
 
 function findWrongBit(array) {
@@ -59,13 +76,11 @@ function correctMessage(wrongBit, array) {
     msj = ''
 
     if (typeof (wrongBit) === 'number') {
-        array[wrongBit - 1] = array[wrongBit - 1] ? '0' : '1'
-        msj += `Mesajul a fost corectat!\nBitul gresit este ${wrongBit}\nMesajul corect este: `
+        array[wrongBit - 1] = array[wrongBit - 1] == 1 ? '0' : '1'
+        return `<p>Mesajul a fost corectat! </p> <p> Bitul gresit este ${wrongBit}</p> <p>Mesajul corect este: ${array.join('')}</p>`
     } else {
-        msj += 'Mesajul initial a fost transmis fara eroare si este: '
+        return `<p>Mesajul a fost transmis fara eroare si este: ${array.join('')} </p>`
     }
-
-    return msj + array.join('')
 }
 
 function initialMessage(array) {
@@ -73,4 +88,176 @@ function initialMessage(array) {
         return !isPowerOfTwo(index + 1)
     }).join('')
 }
+
+function doDecode() {
+    let msj = `${inputDecode.value}`
+    let list = []
+
+    decodeMessages.innerHTML = ''
+    decodeResult.innerHTML = ''
+
+    for (let char of msj) {
+        list.push(char)
+    }
+
+    list.forEach((value, index) => {
+        if (isPowerOfTwo(index + 1)) {
+            let power = Math.log2(index + 1) + 1
+            getNumberOfOnes(index, list)
+        }
+    })
+
+    let wrongBit = findWrongBit(wrongBits)
+
+    messages.forEach(message => {
+        decodeMessages.innerHTML += `${message}`
+    })
+
+    decodeResult.innerHTML = `${correctMessage(wrongBit, list)} ${initialMessage(list)}`
+
+    messages = []
+    wrongBit = ''
+    wrongBits = []
+}
+
+
+/* ****************************************************************
+   ****************************************************************
+   **************************************************************** 
+                    Code
+****************************************************************
+****************************************************************
+******************************************************************/
+
+
+function getControlBitsNumber(msj) {
+    let length = msj.length
+    let numberOfBits = Math.floor(Math.log2(length)) + 2
+
+    return numberOfBits
+}
+
+function completeList(array, controlBitsNumber) {
+    let new_array = []
+    let length = array.length + controlBitsNumber
+    let elementNumber = 0
+
+    for (let i = 0; i < length; i++) {
+        if (isPowerOfTwo(i + 1)) {
+            new_array[i] = ''
+        } else {
+            new_array[i] = array[elementNumber]
+            elementNumber++
+        }
+    }
+
+    if (new_array[new_array.length - 1] == '' || new_array[new_array.length - 1] == undefined) {
+        new_array.pop()
+    }
+
+    return new_array
+}
+
+function findValuesOfC(index, array) {
+    step = index + 1
+    valuesToCheck = step
+    valuesToSkip = step
+    numberOfOnes = 0
+    nameOfVars = ''
+    valueOfVars = ''
+
+    let skip = false
+
+    for (let i = index; i < array.length; i++) {
+        if (valuesToSkip == 0) {
+            skip = false
+        }
+
+        if (skip) {
+            valuesToCheck = step
+            valuesToSkip--
+            continue
+        }
+
+        if (nameOfVars != '') {
+            nameOfVars += ' &#8853; '
+            valueOfVars += ' &#8853; '
+        }
+
+        if (!isPowerOfTwo(i + 1) || i == index) {
+            if (array[i] == 1) {
+                numberOfOnes++
+            }
+
+            console.log(`index = ${i+1}, value = ${array[i]}`);
+            
+
+            if (i == index) {
+                nameOfVars += `c<sub>${i + 1}</sub>`
+                valueOfVars += '___'
+            } else {
+                nameOfVars += `a<sub>${i + 1}</sub>`
+                valueOfVars += `${array[i]}`
+            }
+
+            valuesToCheck--
+        }
+
+        if (valuesToCheck == 0) {
+            skip = true
+            valuesToSkip = step
+        }
+    }
+
+    console.log(numberOfOnes);
+    
+
+    if (numberOfOnes % 2 == 1) {
+        array[index] = 1   
+    } else {
+        array[index] = 0
+    }
+
+    let msj = `<p> ${nameOfVars} = 0  =>  ${valueOfVars} = 0  =>  c<sub>${index+1}</sub> = ${numberOfOnes % 2 == 0 ? '0' : '1'} </p>`
+
+    messages.push(msj)
+}
+
+function getCodeResult(array) {
+    return `Mesajul codificat este: ${array.join('')}`
+}
+
+function doCode() {
+    let msj = `${inputCode.value}`
+    let list = []
+
+    codeMessages.innerHTML = ''
+    codeResult.innerHTML = ''
+
+    for (let char of msj) {
+        list.push(char)
+    }
+
+    controlBitsNumber = getControlBitsNumber(msj)
+
+    list = completeList(list, controlBitsNumber)
+
+    console.log(list);
+
+    list.forEach((value, index) => {
+        if(isPowerOfTwo(index + 1)) {
+            findValuesOfC(index, list)
+        }
+    })
+
+    messages.forEach(message => {
+        codeMessages.innerHTML += `${message}`
+    })
+
+    codeResult.innerHTML = `${getCodeResult(list)}`
+
+    messages = []
+}
+
+
 
--- js/hamming.js
+++ js/hamming.js
@@ -1,24 +1,5 @@
-let msj = '011100101010'
-list = []
-
-for (let char of msj) {
-    list.push(char)
-}
-
-list.forEach((value, index) => {
-    if (isPowerOfTwo(index + 1)) {
-        let power = Math.log2(index + 1) + 1
-        getNumberOfOnes(index, list)
-    }
-})
-
-wrongBit = findWrongBit(wrongBits)
-
-console.log(`${correctMessage(wrongBit, list)}`);
-
-console.log(`${initialMessage(list)}`);
-
-
+executeBtnDecode.addEventListener('click', doDecode)
+executeBtnCode.addEventListener('click', doCode)
 
 
 
